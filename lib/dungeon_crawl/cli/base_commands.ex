2b97a72a4fcd5aedb2206b1477d3df6c4a89827b
Brent Wheeldon <brent@mechanical-orchard.com>
2023-04-18 10:06:21 -0400

    Use with

--- lib/dungeon_crawl/cli/base_commands.ex
+++ lib/dungeon_crawl/cli/base_commands.ex
@@ -1,6 +1,22 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
   alias Mix.Shell.IO, as: Shell
 
+  def ask_for_option(options) do
+    answer =
+      options
+      |> display_options
+      |> generate_question
+      |> Shell.prompt
+
+    with {option, _} <- Integer.parse(answer),
+         chosen when chosen != nil <- Enum.at(options, option - 1) do
+      chosen
+    else
+      :error -> retry(options)
+      nil -> retry(options)
+    end
+  end
+
   def display_options(options) do
     options
     |> Enum.with_index(1)
@@ -16,8 +32,15 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     "Which one? [#{options}]\n"
   end
 
-  def parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
+  def retry(options) do
+    display_error("Invalid option")
+    ask_for_option(options)
+  end
+
+  def display_error(message) do
+    Shell.cmd("clear")
+    Shell.error(message)
+    Shell.prompt("Press enter to continue.")
+    Shell.cmd("clear")
   end
 end
